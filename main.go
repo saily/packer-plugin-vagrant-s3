66fe2241068936612d7b44ad71659c2190782090
Nathan Muir <nathan.muir@3stack.com>
2024-06-04 15:53:53 +1200

    v2.0.1

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterPostProcessor(plugin.DEFAULT_NAME, new(PostProcessor))
-	pps.SetVersion(version.InitializePluginVersion("2.0.0", ""))
+	pps.SetVersion(version.InitializePluginVersion("2.0.1", ""))
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
